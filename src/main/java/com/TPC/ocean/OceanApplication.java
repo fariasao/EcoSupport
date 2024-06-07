245723c7127c1c49737b568bb117133ed4306074
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-06-07 10:39:11 -0300

    Fixed deploy

--- src/main/java/com/TPC/ocean/OceanApplication.java
+++ src/main/java/com/TPC/ocean/OceanApplication.java
@@ -2,19 +2,12 @@ package com.TPC.ocean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OceanApplication extends SpringBootServletInitializer {
+public class OceanApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(OceanApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(OceanApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OceanApplication.class, args);
+	}
 
 }
